75bcffe6c86e63ca83aff6fd3c47f3861f01a02c
SabinaMstan <93779021+SabinaMstan@users.noreply.github.com>
2022-01-29 20:20:11 +0200

    final update
    :)

--- App.js
+++ App.js
@@ -2,18 +2,32 @@ import React, {useState} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import AddWord from './AddWord';
 import SearchWord from './SearchWord';
+import Layout from './Layout';
 
 const App = () => {
   const [dictionary, setDictionary] = useState([]);
-  let found = '';
-  let word = '';
+  const [wordToAdd, setWordToAdd] = useState('');
+
+  const addWordToDictionary = (event) => {
+    event.preventDefault();
+    if (wordToAdd) {
+      setDictionary(dictionary.concat(wordToAdd));
+    }
+    setWordToAdd('');
+  };
+  
  
   return (
     <View style={styles.layout}>
       <Text> Please add a word. {'\n'}</Text>
-      <AddWord onChange ={setDictionary} value = {dictionary} />
+      <Layout 
+        onChange = {setWordToAdd}
+        value = {wordToAdd}
+        onPress = {addWordToDictionary}
+        title = 'Add'
+      /> 
       <Text>{'\n'}</Text>
-      <SearchWord listOfWords = {dictionary} isFound = {found} isWord = {word} />
+      <SearchWord listOfWords = {dictionary} />
     </View>
   )}
 
